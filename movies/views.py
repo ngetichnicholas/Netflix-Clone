8a16ed85845883eda3a225e82e431e1974213bfd
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-08 09:43:50 +0300

    Add home view

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def index(request):
+  popular_movies = get_movies('popular')
+  upcoming_movies = get_movies('upcoming')
+  now_showing_movies = get_movies('now_playing')
+
+  title = 'Netflix | Home'
+
+  search_movie = request.args.get('movie_query')
+
+  if search_movie:
+    return redirect('search')
+
+  else:
+    return render(request,'index.html')
